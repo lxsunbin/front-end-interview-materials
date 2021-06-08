4e007b5b8bf4167697ba55ce7ccb84cd92e499dd
孙彬 <lxsunbin@qq.com>
2021-06-08 22:43:17 +0800

    update

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const p = new Promise((resolve, reject) => {
-	resolve(1);
-});
-p.then(res => {
-	console.log(res);
-	return 2;
-}).then(res => {
-	console.log(res);
-});
-console.log(Object.prototype.toString.call(Promise.resolve('foo')));
+// const p = new Promise((resolve, reject) => {
+// 	resolve(1);
+// });
+// p.then(res => {
+// 	console.log(res);
+// 	return 2;
+// }).then(res => {
+// 	console.log(res);
+// });
+// console.log(Object.prototype.toString.call(Promise.resolve('foo')));
